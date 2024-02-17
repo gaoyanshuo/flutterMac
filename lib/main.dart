e4004bbc3d5d4b1f61a6993338d796d6b791ccfd
kou <yanshuo.gao@i-focus.co.jp>
2024-02-18 02:12:36 +0900

    Container Text

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main(List<String> args) {
         appBar: AppBar(
           title: const Text('Hello, flutter!'),
         ),
-        body: const MyApp()),
+        body: const Column(
+          children: [MyApp(), MyButton(), MyText()],
+        )),
   ));
 }
 
@@ -18,8 +20,10 @@ class MyApp extends StatelessWidget {
     return Center(
       child: Container(
         alignment: Alignment.center, // 控制container内， 元素的方位
+        margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
         height: 200,
         width: 200,
+        transform: Matrix4.translationValues(10, 0, 0),
         decoration: BoxDecoration(
             color: Colors.blue,
             border: Border.all(color: Colors.green, width: 10),
@@ -29,6 +33,46 @@ class MyApp extends StatelessWidget {
           'HAHA',
           style: TextStyle(color: Colors.pink, fontSize: 20),
         ),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 40,
+      alignment: Alignment.center,
+      margin: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+          color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+      child: const Text(
+        'button',
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
+  }
+}
+
+class MyText extends StatelessWidget {
+  const MyText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: const BoxDecoration(color: Colors.yellow),
+      child: const Text(
+        'I am Text Widget. I am Text Widget.',
+        textAlign: TextAlign.center,
+        overflow: TextOverflow.ellipsis, // ...
+        maxLines: 1,
+        style: TextStyle(fontSize: 20),
       ),
     );
   }
